75b44e578d5613398544e267cabab09c36621fe8
Mischa Rouleaux <mischa-rouleaux@live.nl>
2017-10-08 16:30:19 +0200

    Added the documentation

--- Sokoban/SokobanTest/Program.cs
+++ Sokoban/SokobanTest/Program.cs
@@ -25,6 +25,120 @@ namespace SokobanSolver
 		private string destBoard, currBoard;
 		private int playerX, playerY, nCols;
 
+		public static void Main(string[] a)
+		{
+			string level2 = "#######," +
+						   "#     #," +
+						   "#     #," +
+						   "#. #  #," +
+						   "#. $$ #," +
+						   "#.$$  #," +
+						   "#.#  @#," +
+						   "#######";
+
+			string level3 = "########," +
+						  "#  #  ##," +
+						  "# $@$  #," +
+						  "#. #  .#," +
+						  "########";
+
+			string level4 = "####," +
+							"#@.#," +
+							"#$ #," +
+							"#  #," +
+							"# $#," +
+							"#. #," +
+							"#  #," +
+							"####";
+
+			string level5 = "########," +
+							"####   #," +
+							"#@$  # #," +
+							"#. $  .#," +
+							"########";
+
+			string level = "########," +
+						   "#@ #   #," +
+						   "#  #.  #," +
+						   "#  # $ #," +
+						   "## $ ###," +
+						   "##   ###," +
+						   "##.  ###," +
+						   "########";
+
+
+
+			System.Console.WriteLine("Level:\n");
+			foreach (string line in level.Split(','))
+			{
+				System.Console.WriteLine(line);
+			}
+			System.Console.WriteLine("\nSolution:\n");
+			var sol = new SokobanSolver(level.Split(',')).Solve();
+			System.Console.WriteLine(sol);
+			System.Console.Write(sol.Count());
+		}
+
+		private string Solve()
+		{
+			char[,] dirLabels = { { 'u', 'U' }, { 'r', 'R' }, { 'd', 'D' }, { 'l', 'L' } };
+			int[,] dirs = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+			ISet<string> history = new HashSet<string>();
+			LinkedList<Board> open = new LinkedList<Board>();
+
+			history.Add(currBoard);
+			open.AddLast(new Board(currBoard, string.Empty, playerX, playerY));
+
+			while (!open.Count.Equals(0))
+			{
+				Board item = open.First();
+				open.RemoveFirst();
+				string cur = item.Cur;
+				string sol = item.Sol;
+				int x = item.X;
+				int y = item.Y;
+
+				for (int i = 0; i < dirs.GetLength(0); i++)
+				{
+					string trial = cur;
+					int dx = dirs[i, 0];
+					int dy = dirs[i, 1];
+
+					// are we standing next to a box ?
+					if (trial[(y + dy) * nCols + x + dx] == '$')
+					{
+						// can we push it ?
+						if ((trial = Push(x, y, dx, dy, trial)) != null)
+						{
+							// or did we already try this one ?
+							if (!history.Contains(trial))
+							{
+
+								string newSol = sol + dirLabels[i, 1];
+
+								if (IsSolved(trial))
+									return newSol;
+
+								open.AddLast(new Board(trial, newSol, x + dx, y + dy));
+								history.Add(trial);
+							}
+						}
+						// otherwise try changing position
+					}
+					else if ((trial = Move(x, y, dx, dy, trial)) != null)
+					{
+						if (!history.Contains(trial))
+						{
+							string newSol = sol + dirLabels[i, 0];
+							open.AddLast(new Board(trial, newSol, x + dx, y + dy));
+							history.Add(trial);
+						}
+					}
+				}
+			}
+			return "No solution";
+		}
+
 		SokobanSolver(string[] board)
 		{
 			nCols = board[0].Length;
@@ -90,120 +204,6 @@ namespace SokobanSolver
 						!= (trialBoard[i] == '$'))
 					return false;
 			return true;
-		}
-
-		private string Solve()
-		{
-			char[,] dirLabels = { { 'u', 'U' }, { 'r', 'R' }, { 'd', 'D' }, { 'l', 'L' } };
-			int[,] dirs = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
-			ISet<string> history = new HashSet<string>();
-			LinkedList<Board> open = new LinkedList<Board>();
-
-			history.Add(currBoard);
-			open.AddLast(new Board(currBoard, string.Empty, playerX, playerY));
-
-			while (!open.Count.Equals(0))
-			{
-				Board item = open.First();
-				open.RemoveFirst();
-				string cur = item.Cur;
-				string sol = item.Sol;
-				int x = item.X;
-				int y = item.Y;
-
-				for (int i = 0; i < dirs.GetLength(0); i++)
-				{
-					string trial = cur;
-					int dx = dirs[i, 0];
-					int dy = dirs[i, 1];
-
-					// are we standing next to a box ?
-					if (trial[(y + dy) * nCols + x + dx] == '$')
-					{
-						// can we push it ?
-						if ((trial = Push(x, y, dx, dy, trial)) != null)
-						{
-							// or did we already try this one ?
-							if (!history.Contains(trial))
-							{
-
-								string newSol = sol + dirLabels[i, 1];
-
-								if (IsSolved(trial))
-									return newSol;
-
-								open.AddLast(new Board(trial, newSol, x + dx, y + dy));
-								history.Add(trial);
-							}
-						}
-						// otherwise try changing position
-					}
-					else if ((trial = Move(x, y, dx, dy, trial)) != null)
-					{
-						if (!history.Contains(trial))
-						{
-							string newSol = sol + dirLabels[i, 0];
-							open.AddLast(new Board(trial, newSol, x + dx, y + dy));
-							history.Add(trial);
-						}
-					}
-				}
-			}
-			return "No solution";
-		}
-
-		public static void Main(string[] a)
-		{
-			string level2 = "#######," +
-						   "#     #," +
-						   "#     #," +
-						   "#. #  #," +
-						   "#. $$ #," +
-						   "#.$$  #," +
-						   "#.#  @#," +
-						   "#######";
-            
-            string level3 = "########," +
-                          "#  #  ##," +
-                          "# $@$  #," +
-                          "#. #  .#," +
-                          "########";
-
-            string level4 = "####," +
-                            "#@.#," +
-                            "#$ #," +
-                            "#  #," +
-                            "# $#," +
-                            "#. #," +
-                            "#  #," +
-                            "####";
-
-            string level5 = "########," +
-                            "####   #," +
-                            "#@$  # #," +
-                            "#. $  .#," +
-                            "########";
-            
-            string level = "########," +
-                           "#@ #   #," +
-                           "#  #.  #," +
-                           "#  # $ #," +
-                           "## $ ###," +
-                           "##   ###," +
-                           "##.  ###," +
-                           "########";
-
-
-
-			System.Console.WriteLine("Level:\n");
-			foreach (string line in level.Split(','))
-			{
-				System.Console.WriteLine(line);
-			}
-			System.Console.WriteLine("\nSolution:\n");
-            var sol = new SokobanSolver(level.Split(',')).Solve();
-            System.Console.WriteLine(sol);
-            System.Console.Write(sol.Count());
 		}
 	}
 }
